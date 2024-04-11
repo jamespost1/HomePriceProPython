db4347f1ad5ef7375f2fbf9777fe880dee659185
alyssacastil <86081209+alyssacastil@users.noreply.github.com>
2024-04-11 14:44:42 +0000

    Adding Forest class

---
+++ Forest.java
@@ -0,0 +1,21 @@
+public class Forest {
+
+//attributes
+    boolean profit;
+    Node forestRoot;
+    vector trees;
+    boolean finalPrediction;
+
+//methods
+    static void growTree() {
+    System.out.println("Hello World!");
+  }
+
+  static void majorityTreeVote() {
+    System.out.println("Hello World!");
+  }
+
+  static void calculateProfit() {
+    System.out.println("Hello World!");
+  }
+}//end of Forest Class
